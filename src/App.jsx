b9771182393367961ca3ab3e8c8ba20998cc65d5
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-13 20:35:02 -0400

    imported data and stored in state constant

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
+import { useState } from 'react';
+import data from './data';
 
 function App() {
+  const [people, setPeople] = useState(data);
 
   return (
     <div>
