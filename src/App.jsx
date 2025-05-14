7c5192e3d95a21c2f4de2f49c304ef159f86bfbe
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-13 20:42:14 -0400

    Mapped through people contained in data

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import './App.css'
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import data from './data';
 
 function App() {
   const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
 
   return (
-    <div>
-      App
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews  
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          // more stuff coming
+          return (
+            <article key={id}>
+              <img src={image} alt={name} />
+            </article>
+          );
+        })}
+      </div>
+    </section>
   )
 }
 
